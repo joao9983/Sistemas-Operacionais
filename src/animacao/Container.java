0239fd9c18c1a3d5e4d25ae84f11ae8fc8b34300
jbrun0r <jbrun0rjbrun0r@gmail.com>
2022-04-13 11:32:07 -0300

    cmmt "está consumado!"
    Projeto Final Consumado.
    
    Co-Authored-By: Gabriel Costa <56612216+brielsilva@users.noreply.github.com>
    Co-Authored-By: João Carlos Nepomuceno Fernandes <72822834+joao9983@users.noreply.github.com>

---
+++ src/animacao/Container.java
@@ -0,0 +1,29 @@
+package animacao;
+
+import javax.swing.JFrame;
+
+import animacao_modelo.Fase;
+
+public class Container extends JFrame {
+	public Container() {
+		
+		add(new Fase());
+		setTitle("Anime");
+		setSize(1024, 750);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String []args) {
+		new Container();
+		
+		//Fase f = new Fase();
+		//f.setSinal(true);
+	}
+	
+	public void ChamaTrem() {
+		add(new Fase());
+	}
+}
